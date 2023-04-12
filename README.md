e8cb01828710034ef714e812d1e2d218e3fc41a7
chmilevfa <17650184+ChmilevFA@users.noreply.github.com>
2023-04-12 16:20:30 +0200

    test deploy

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mcoin-contracts
+
+To deploy:
+```shell
+npx hardhat run scripts/deploy.ts --network polygonMumbai
+npx hardhat verify --network polygonMumbai DEPLOYED_CONTRACT_ADDRESS "Constructor argument 1"
+```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
+const POLYGON_SCAN_API_KEY = process.env.POLYGON_SCAN_API_KEY;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    polygonMumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${WALLET_PRIVATE_KEY}`]
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: `${POLYGON_SCAN_API_KEY}`,
+    },
+  },
 };
 
 export default config;
